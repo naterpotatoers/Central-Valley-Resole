1c3713dc615431725638635c2567064a2b0df2d0
naterpotatoer <nate.garza@hotmail.com>
2024-09-01 22:45:41 -0700

    :sparkles: initial setup

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Central Valley Resole';
+export const SITE_DESCRIPTION = 'Climbing shoe resoling service based out of the Central Valley of California.';
+export const SITE_URL = 'centralvalleyresole.com';
+export const SITE_IMAGE = '/og.png';
